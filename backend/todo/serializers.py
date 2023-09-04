92daca49bfe3f833ffb409c7e0de3a4b3851c833
Charan Krishna <charannori.0401@gmail.com>
2023-09-04 19:39:52 +1000

    django

---
+++ backend/todo/serializers.py
@@ -0,0 +1,7 @@
+﻿from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
